1ed7db3e9d9344b4c0428a76f7204904345d2dee
sprevaux2026 <stevenprevaux@gmail.com>
2025-02-16 09:45:18 -0500

    Task 5 - Loan Payment Calculation

--- cc_8.js
+++ cc_8.js
@@ -60,3 +60,14 @@ function calculateRentalCost(days, carType, insurance = false) {
 // Data and logging
 console.log(`Total Rental Cost: $${calculateRentalCost(3, "Economy", true)}`);
 console.log(`Total Rental Cost: $${calculateRentalCost(5, "Luxury", false)}`);
+
+// Task 5: Returning Values - Loan Payment Calculation
+// calculate the total payment for a loan based on principal, rate, and time
+function calculateLoanPayment(principal, rate, time) {
+    let totalPayment = principal + (principal * rate * time);
+    return totalPayment;
+}
+
+// Data and logging
+console.log(`Total Payment: $${calculateLoanPayment(1000, 0.05, 2).toFixed(2)}`);
+console.log(`Total Payment: $${calculateLoanPayment(5000, 0.07, 3).toFixed(2)}`);
