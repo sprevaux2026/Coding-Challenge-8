4ddd92c34c9487f0f86b98a52d3066b9402a37ca
sprevaux2026 <stevenprevaux@gmail.com>
2025-02-16 09:43:33 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -8,3 +8,14 @@ function calculateSalary(baseSalary, bonus, taxRate) {
 // Test Data and logging
 console.log(`Net Salary: $${calculateSalary(5000, 500, 0.1).toFixed(2)}`);
 console.log(`Net Salary: $${calculateSalary(7000, 1000, 0.15).toFixed(2)}`);
+
+// Task 2: Function Expression - Product Price After Discount
+// calculate the final price of a product after discount
+const calculateDiscount = function(price, discountRate) {
+    let finalPrice = price - (price * discountRate);
+    return finalPrice;
+};
+
+// Data and logging
+console.log(`Final Price: $${calculateDiscount(100, 0.2).toFixed(2)}`);
+console.log(`Final Price: $${calculateDiscount(250, 0.15).toFixed(2)}`);
